c092064a512ac831d65afe8daf1285bbf6ed4348
Unzor <71789103+Unzor@users.noreply.github.com>
2021-10-19 07:17:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const _ = require('lodash');
+
+const app = express();
+
+// enable files upload
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+//add other middleware
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
 app.get('/', function(req, res){
 res.send('yes');
 });
 app.post('/api/add', function(req, res){
-    var fs = require('fs');
-    var game_buffer=fs.readFileSync(req.body.path);
+    var h = req.files;
+    var game_buffer=h.fileUploaded.data;
  app.get('/games/' + req.body.name, function(req, res){
  res.send(game_buffer);
  });
  res.send({success: true, url: '/games/' + req.body.name});
 });
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log('Express server listening on port', port)
+app.listen(5050, function(){
+    console.log('App listening!');
 });
